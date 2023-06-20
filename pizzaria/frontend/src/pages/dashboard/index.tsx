c1af70effab2442a0054e56313a50cd0239ea9b7
champs-8 <https://github.com/champs-8/Projetos>
2023-06-19 22:15:39 -0300

    criando a tela de dashboard

--- pizzaria/frontend/src/pages/dashboard/index.tsx
+++ pizzaria/frontend/src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { canSSRAuth } from "@/utils/canSSRAuth";
 import Head from "next/head";
+import styles from './styles.module.scss'
 import {Header} from '../../components/Header/index'
+import {FiRefreshCcw} from 'react-icons/fi'
 
 export default function Dashboard() {
     return (
@@ -10,7 +12,23 @@ export default function Dashboard() {
         </Head>
         <div>
             <Header/>
-            <h1>Painel</h1>
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1>Ultimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw color="#3fffa3" size={25}/>
+                    </button>
+                </div>
+
+                <article className={styles.listOrders}>
+                    <section className={styles.orderItem}>
+                        <button>
+                            <div className={styles.tag}></div>
+                            <span>Mesa 30</span>
+                        </button>
+                    </section>
+                </article>
+            </main>
         </div>
         </>
     );
