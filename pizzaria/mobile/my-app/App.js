1aa4392622ea8f17393cd79a1ed1338e67a8e61f
champs-8 <https://github.com/champs-8/Projetos>
2023-07-17 23:04:51 -0300

    inicio do projeto mobile

--- pizzaria/mobile/my-app/App.js
+++ pizzaria/mobile/my-app/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text} from "react-native"
+// view é como se fosse a div, um espaço em branco
+//o text é para poder escrever texto
+import React from "react"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function app(){
+  return(
+    <View style={{flex:1, backgroundColor:'#505050', paddingTop: 25}}>
+      {/* ja vem com o flexbox ativado 
+      flex 1 faz ocupar a tela toda*/}
+
+      <Text style={{fontSize: 28}}>ChamPizza</Text>
+      <Text>Seja bem-vindo</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
