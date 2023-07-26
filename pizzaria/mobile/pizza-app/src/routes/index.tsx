a76e80116c0dc5a6e957eac240e30ebce14714ed
champs-8 <https://github.com/champs-8/Projetos>
2023-07-26 12:10:48 -0300

    configurando tela de login

--- pizzaria/mobile/pizza-app/src/routes/index.tsx
+++ pizzaria/mobile/pizza-app/src/routes/index.tsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, ActivityIndicator } from "react-native";
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
 export default function Routes() {
 
     const isAuthenticated = false;
-    const loading = false;
+    const [loading, setLoading] = useState(false);
+    
+    if(loading) {
+        return(
+            <View 
+                style={{
+                    flex: 1,
+                    backgroundColor: '#1d1d2e',
+                    justifyContent:'center',
+                    alignItems:'center'
+
+                }}
+            >
+                <ActivityIndicator size={60} color={'#f5f7fb'}/>
+            </View>
+        );
+    }
 
     return(
         isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
