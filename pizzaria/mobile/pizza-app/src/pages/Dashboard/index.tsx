d7e0e0c30d205bd81007d4c46af33b7691fbbe79
champs-8 <https://github.com/champs-8/Projetos>
2023-08-07 22:48:49 -0300

    navegacao da tela da mesa para pedidos

--- pizzaria/mobile/pizza-app/src/pages/Dashboard/index.tsx
+++ pizzaria/mobile/pizza-app/src/pages/Dashboard/index.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import {Text, SafeAreaView, TouchableOpacity, TextInput , StyleSheet} from "react-native";
+//para poder navegar entre os componentes
+import { useNavigation } from "@react-navigation/native";
+import {NativeStackNavigationProp} from '@react-navigation/native-stack'
+import {StackParamsList} from '../../routes/app.routes'
 
 export default function Dashboard() {
+
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+    //a tipagem tem outra tipagem tambem
+
+    const [table, setTable] = useState('')
+
+    async function openOrder() {
+        if(table === '') {
+            return;
+        }
+
+        //precisa fazer a requisição e abrir a mesa e navegar para a proxima tela
+        //tem que passar dois params, sao configurados na tipagem
+        navigation.navigate('Order', {table: table, order_id: ''})
+    }
 
     return(
 
@@ -14,9 +33,11 @@ export default function Dashboard() {
                 placeholderTextColor={'#cecece'}
                 style = {styles.input}
                 keyboardType="numeric"
+                value={table}
+                onChangeText={setTable}
             />
 
-            <TouchableOpacity style= {styles.button}>
+            <TouchableOpacity style= {styles.button} onPress={openOrder}>
                 <Text style={styles.buttonText}>Abrir mesa</Text>
             </TouchableOpacity>
         </SafeAreaView>
